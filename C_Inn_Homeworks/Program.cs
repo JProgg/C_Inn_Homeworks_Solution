ee5fe4398bfa594079c605d8202820bf8dbb7512
JProgg <jprog@protonmail.com>
2020-01-15 22:13:31 +0100

    Revert "Fixed for codacy"
    This reverts commit 1755563a365dfa63cef135dbb84d479fc8d0a06b.

--- C_Inn_Homeworks/Program.cs
+++ C_Inn_Homeworks/Program.cs
@@ -1,9 +1,8 @@
 ﻿using System;
-using System.Diagnostics;
 
 namespace C_Inn_Homeworks
 {
-    public static class Program
+    public class Program
     {
         
 
@@ -17,26 +16,26 @@ namespace C_Inn_Homeworks
 
             for (int i = 0; i <= 1; i++)
             {
-                Debug.Write("Please, tell me your name: ");
+                Console.Write("Please, tell me your name: ");
                 name = Console.ReadLine();
 
-                Debug.Write("Please, tell me your surname: ");
+                Console.Write("Please, tell me your surname: ");
                 surname = Console.ReadLine();
 
-                Debug.Write("Please, tell me your age: ");
+                Console.Write("Please, tell me your age: ");
                 age = Convert.ToInt32(Console.ReadLine());
 
-                Debug.Write("Please, tell me your weight: ");
+                Console.Write("Please, tell me your weight: ");
                 weight = Convert.ToDouble(Console.ReadLine());
 
-                Debug.Write("Please, tell me your height (in cm): ");
+                Console.Write("Please, tell me your height (in cm): ");
                 height = Convert.ToDouble(Console.ReadLine());
 
-                Debug.WriteLine("\n{0} {1} is {2} years old, his weight is {3} kg and his height is {4} cm.", name, surname, age, weight, height);
+                Console.WriteLine("\n{0} {1} is {2} years old, his weight is {3} kg and his height is {4} cm.", name, surname, age, weight, height);
 
                 double BMI = Math.Round(CalculateBMI(weight, height), 2);
 
-                Debug.WriteLine("His BMI is: {0} \n", BMI); 
+                Console.WriteLine("His BMI is: {0} \n", BMI); 
             }
 
         }
