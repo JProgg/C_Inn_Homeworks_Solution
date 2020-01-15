52aaad59fce7c6428fd9cd43b3ef1bb61474d978
JProgg <jprog@protonmail.com>
2020-01-15 21:46:29 +0100

    First version

---
+++ C_Inn_Homeworks/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace C_Inn_Homeworks
+{
+    public class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            string name;
+            string surname = "";
+            int age = 0;
+            double weight = 0;
+            double height = 0;
+
+            for (int i = 0; i <= 1; i++)
+            {
+                Console.Write("Please, tell me your name: ");
+                name = Console.ReadLine();
+
+                Console.Write("Please, tell me your surname: ");
+                surname = Console.ReadLine();
+
+                Console.Write("Please, tell me your age: ");
+                age = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Please, tell me your weight: ");
+                weight = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Please, tell me your height (in cm): ");
+                height = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("\n{0} {1} is {2} years old, his weight is {3} kg and his height is {4} cm.", name, surname, age, weight, height);
+
+                double BMI = Math.Round(CalculateBMI(weight, height), 2);
+
+                Console.WriteLine("His BMI is: {0} \n", BMI); 
+            }
+
+        }
+        static double CalculateBMI(double weight, double height)
+        {
+            return weight / ( Math.Pow((height / 100), 2));
+        }
+
+    }
+}
